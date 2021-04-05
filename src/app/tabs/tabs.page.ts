a1be40c446729157b86134145902e0a7c78f48d0
Dina Gueye <dina3903@gmail.com>
2021-04-05 16:07:07 +0000

    Design the tab bar

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from "@angular/core";
+import { IonTabs } from "@ionic/angular";
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  selector: "app-tabs",
+  templateUrl: "tabs.page.html",
+  styleUrls: ["tabs.page.scss"],
 })
 export class TabsPage {
+  @ViewChild("tabs", { static: false }) tabs: IonTabs;
+  selectedTab: any;
+  homeIcon: String = "home-outline";
+  categorieIcon: String = "search-outline";
+  favorisIcon: String = "heart-outline";
+  profilIcon: String = "person-outline";
 
   constructor() {}
 
+  setCurrentTab() {
+    this.resetIcon();
+    this.selectedTab = this.tabs.getSelected();
+    this.fillIcon();
+  }
+
+  resetIcon() {
+    this.homeIcon = "home-outline";
+    this.categorieIcon = "search-outline";
+    this.favorisIcon = "heart-outline";
+    this.profilIcon = "person-outline";
+  }
+
+  fillIcon() {
+    if (this.selectedTab == "home") {
+      this.homeIcon = "home";
+    }
+    if (this.selectedTab == "categorie") {
+      this.categorieIcon = "search";
+    }
+    if (this.selectedTab == "favoris") {
+      this.favorisIcon = "heart";
+    }
+    if (this.selectedTab == "plus") {
+      this.profilIcon = "person";
+    }
+  }
 }
