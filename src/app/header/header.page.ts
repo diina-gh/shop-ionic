a60905fba9e52e3c688c4c90c95d89c84c563a49
Dina Gueye <dina3903@gmail.com>
2021-04-28 22:48:31 +0000

    Add searchbar

--- src/app/header/header.page.ts
+++ src/app/header/header.page.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from '@ionic/angular'
+import {NavController,} from '@ionic/angular'
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.page.html',
-  styleUrls: ['./header.page.scss'],
+  selector: "app-header",
+  templateUrl: "./header.page.html",
+  styleUrls: ["./header.page.scss"],
 })
 export class HeaderPage implements OnInit {
+  searching: Boolean = false;
+  href: any;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private router: Router) {
+  }
 
   ngOnInit() {
+    this.checkPage();
   }
 
-  toHome(){
+  checkPage() {
+    this.href = this.router.url;
+    console.log("We here", this.href);
+    if (this.href == "/tabs/categorie") {
+      console.log("YES YES");
+      this.searching = true;
+    } else {
+      console.log("NOPE");
+      this.searching = false;
+    }
+  }
+
+  toHome() {
     this.navCtrl.navigateBack("tabs/home");
   }
-
 }
